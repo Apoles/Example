3ea61bf4f213e574a6a2ea90acc9348f0083997a
Apoles <emine176gumus@gmail.com>
2024-02-21 15:42:01 +0300

    a

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from '@nextui-org/react';
 import Image from 'next/image.js';
+import Link from 'next/link';
 
 const MyNavbar = () => {
   return (
     <Navbar className=' w-1/2 p-0  ' shouldHideOnScroll>
       <NavbarBrand className='object-left-top  flex flex-row items-start  '>
-        <Image className='' src={`/logo.png`} alt='logo' width='252' height='50' />
+        <Link className='' href={'/'}>
+          <Image className='' src={`/logo.png`} alt='logo' width='252' height='50' />
+        </Link>
       </NavbarBrand>
     </Navbar>
   );
